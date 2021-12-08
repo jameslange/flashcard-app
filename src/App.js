070c30474c3da2583526ba7ec45df3f2a8006a2d
jameslange <jlange101592@gmail.com>
2021-12-08 15:32:20 +0100

    hashRouter

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import CardEditor from './components/CardEditor';
 import CardViewer from './components/CardViewer';
 
 import './App.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {HashRouter, Routes, Route} from 'react-router-dom';
 import Home from './components/Home';
 
 function App() {
   const[listOfCards, setListOfCards] = useState([]);
+
+  const initialState={
+    cardFront:0,
+    cardBack:0,
+    listOfCards:[],
+  }
+
 
   useEffect(()=>{
     if((localStorage.getItem('listOfCards')!== null)&&(localStorage.getItem('listOfCards')!== "")){
@@ -26,13 +33,13 @@ function App() {
   return (
     <>
   
-     <BrowserRouter>
+     <HashRouter>
      <Routes>
     <Route path="/" element={<Home />} />
         <Route index element={<CardEditor listOfCards={listOfCards} setListOfCards={setListOfCards}/>}/>
         <Route path="cardviewer" element={<CardViewer setListOfCards={setListOfCards}listOfCards={listOfCards} />} />
     </Routes >
-</BrowserRouter>
+</HashRouter>
 </>
   );
 }
